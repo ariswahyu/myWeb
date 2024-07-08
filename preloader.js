1e911c97247e93b36d732b17c5d0f8ce25a66b13
ariswahyu <40571729+ariswahyu@users.noreply.github.com>
2024-07-08 18:31:07 +0700

    Create preloader.js

---
+++ preloader.js
@@ -0,0 +1,57 @@
+<script>
+  var _0x12859f = _0x1d35;
+  (function(_0x295fc1, _0xae5b62) {
+    var _0x45f1d2 = _0x1d35,
+      _0x8f2b0f = _0x295fc1();
+    while (!![]) {
+      try {
+        var _0x299064 = parseInt(_0x45f1d2(0x142)) / 0x1 + -parseInt(_0x45f1d2(0x159)) / 0x2 * (parseInt(_0x45f1d2(0x15d)) / 0x3) + -parseInt(_0x45f1d2(0x144)) / 0x4 * (parseInt(_0x45f1d2(0x143)) / 0x5) + -parseInt(_0x45f1d2(0x14b)) / 0x6 + -parseInt(_0x45f1d2(0x145)) / 0x7 + parseInt(_0x45f1d2(0x146)) / 0x8 * (-parseInt(_0x45f1d2(0x151)) / 0x9) + parseInt(_0x45f1d2(0x148)) / 0xa;
+        if (_0x299064 === _0xae5b62) break;
+        else _0x8f2b0f['push'](_0x8f2b0f['shift']());
+      } catch (_0x5a0bc1) {
+        _0x8f2b0f['push'](_0x8f2b0f['shift']());
+      }
+    }
+  }(_0x521c, 0x35a00));
+
+  function _0x521c() {
+    var _0x24c599 = ['DOMContentLoaded', 'CSS', 'noplay', 'undefined', '205346NQPrIR', 'remove', 'querySelectorAll', 'add', '3upHMuj', '320487zHdnJh', '26805vGHqCJ', '100kEEoqX', '118650qQuhdK', '16fJTetG', 'webkitHidden', '6668560xDiGtY', 'forEach', 'supports', '1732278BIQcDZ', 'msHidden', 'addEventListener', 'animation-name', 'webkitvisibilitychange', 'supportsCSS', '1014003haKsIU', 'hidden', 'visibilitychange', 'classList'];
+    _0x521c = function() {
+      return _0x24c599;
+    };
+    return _0x521c();
+  }
+
+  function _0x1d35(_0x33e05b, _0x4fd27b) {
+    var _0x521c7b = _0x521c();
+    return _0x1d35 = function(_0x1d3539, _0x18a2ed) {
+        _0x1d3539 = _0x1d3539 - 0x142;
+        var _0x5cf7dd = _0x521c7b[_0x1d3539];
+        return _0x5cf7dd;
+      },
+      _0x1d35(_0x33e05b, _0x4fd27b);
+  }
+  var supportsCSS = !!(window['CSS'] && window[_0x12859f(0x156)][_0x12859f(0x14a)] || window[_0x12859f(0x150)] || ![]);
+  if (supportsCSS && CSS[_0x12859f(0x14a)](_0x12859f(0x14e), 'test')) {
+    var hidden, visibilityChange;
+    if (typeof document[_0x12859f(0x152)] !== 'undefined') hidden = _0x12859f(0x152),
+      visibilityChange = _0x12859f(0x153);
+    else {
+      if (typeof document[_0x12859f(0x14c)] !== 'undefined') hidden = _0x12859f(0x14c),
+        visibilityChange = 'msvisibilitychange';
+      else typeof document[_0x12859f(0x147)] !== _0x12859f(0x158) && (hidden = _0x12859f(0x147), visibilityChange = _0x12859f(0x14f));
+    }
+    let introElements = document[_0x12859f(0x15b)]('.sp_preloader\x20*');
+    document[hidden] ? introElements[_0x12859f(0x149)](_0x248ed9 => _0x248ed9[_0x12859f(0x154)][_0x12859f(0x15c)](_0x12859f(0x157))) : introElements[_0x12859f(0x149)](_0x536e63 => _0x536e63[_0x12859f(0x154)][_0x12859f(0x15a)]('noplay'));
+
+    function manageVisibilityChange() {
+      var _0x20d6b7 = _0x12859f;
+      document[hidden] ? introElements[_0x20d6b7(0x149)](_0x528107 => _0x528107[_0x20d6b7(0x154)]['add'](_0x20d6b7(0x157))) : introElements[_0x20d6b7(0x149)](_0xa93e1d => _0xa93e1d[_0x20d6b7(0x154)][_0x20d6b7(0x15a)](_0x20d6b7(0x157)));
+    }
+    document[_0x12859f(0x14d)](visibilityChange, manageVisibilityChange, ![]),
+      document[_0x12859f(0x14d)](_0x12859f(0x155), function() {
+        var _0x3cef2d = _0x12859f;
+        introElements[_0x3cef2d(0x149)](_0x1bdc6b => _0x1bdc6b[_0x3cef2d(0x154)][_0x3cef2d(0x15c)]('playanim'));
+      });
+  }
+</script>
